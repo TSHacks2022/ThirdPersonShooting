909f44f610a4da6c4403194896fc6ac4c2df69ae
yuma <56620459+yuma-35@users.noreply.github.com>
2022-08-11 15:47:18 +0900

    add inventory

---
+++ Assets/FPS/Scripts/UI/InvPanelManager.cs
@@ -0,0 +1,34 @@
+﻿using Unity.FPS.Game;
+using Unity.FPS.Gameplay;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Unity.FPS.UI
+{
+    public class InvPanelManager : MonoBehaviour
+    {
+
+        [SerializeField] GameObject inventoryPanel;
+        [SerializeField] TextMeshProUGUI inventoryText;
+
+        void Start()
+        {
+            inventoryPanel.SetActive(false);
+        }
+
+        void Update()
+        {
+            inventoryText.text = ItemManager.instance.GetInventory();
+
+            //　左のシフトキーをキーコードで判断
+            if(Input.GetKey(KeyCode.LeftShift)) {
+                Debug.Log(ItemStaticData.potion);
+                inventoryPanel.SetActive(true);
+            }else{
+                inventoryPanel.SetActive(false);
+            }
+        }
+    }
+}
