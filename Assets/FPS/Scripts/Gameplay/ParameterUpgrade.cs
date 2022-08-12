75964f34cbb8c6dae86629f95c0dce235f3fd7eb
shoji03 <shoji-manato-vn@ynu.jp>
2022-08-12 14:13:04 +0900

    xx

---
+++ Assets/FPS/Scripts/Gameplay/ParameterUpgrade.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Unity.FPS.Gameplay
+{
+    
+    public class ParameterManager : MonoBehaviour
+    {
+
+        Health m_PlayerHealth;
+
+        void Start()
+        {
+            PlayerCharacterController playerCharacterController =
+                GameObject.FindObjectOfType<PlayerCharacterController>();
+
+            playerCharacterController.MaxSpeedOnGround += 10;
+            playerCharacterController.MaxSpeedInAir += 10;
+
+        }
+
+    }
+}
--- Assets/FPS/Scripts/UI/JetpackCounter.cs
+++ Assets/FPS/Scripts/UI/JetpackCounter.cs
@@ -1,40 +1,40 @@
-﻿using Unity.FPS.Game;
-using Unity.FPS.Gameplay;
-using UnityEngine;
-using UnityEngine.UI;
+﻿// using Unity.FPS.Game;
+// using Unity.FPS.Gameplay;
+// using UnityEngine;
+// using UnityEngine.UI;
 
-namespace Unity.FPS.UI
-{
-    public class JetpackCounter : MonoBehaviour
-    {
-        [Tooltip("Image component representing jetpack fuel")]
-        public Image JetpackFillImage;
+// namespace Unity.FPS.UI
+// {
+//     public class JetpackCounter : MonoBehaviour
+//     {
+//         [Tooltip("Image component representing jetpack fuel")]
+//         public Image JetpackFillImage;
 
-        [Tooltip("Canvas group that contains the whole UI for the jetack")]
-        public CanvasGroup MainCanvasGroup;
+//         [Tooltip("Canvas group that contains the whole UI for the jetack")]
+//         public CanvasGroup MainCanvasGroup;
 
-        [Tooltip("Component to animate the color when empty or full")]
-        public FillBarColorChange FillBarColorChange;
+//         [Tooltip("Component to animate the color when empty or full")]
+//         public FillBarColorChange FillBarColorChange;
 
-        Jetpack m_Jetpack;
+//         Jetpack m_Jetpack;
 
-        void Awake()
-        {
-            m_Jetpack = FindObjectOfType<Jetpack>();
-            DebugUtility.HandleErrorIfNullFindObject<Jetpack, JetpackCounter>(m_Jetpack, this);
+//         void Awake()
+//         {
+//             m_Jetpack = FindObjectOfType<Jetpack>();
+//             DebugUtility.HandleErrorIfNullFindObject<Jetpack, JetpackCounter>(m_Jetpack, this);
 
-            FillBarColorChange.Initialize(1f, 0f);
-        }
+//             FillBarColorChange.Initialize(1f, 0f);
+//         }
 
-        void Update()
-        {
-            MainCanvasGroup.gameObject.SetActive(m_Jetpack.IsJetpackUnlocked);
+//         void Update()
+//         {
+//             MainCanvasGroup.gameObject.SetActive(m_Jetpack.IsJetpackUnlocked);
 
-            if (m_Jetpack.IsJetpackUnlocked)
-            {
-                JetpackFillImage.fillAmount = m_Jetpack.CurrentFillRatio;
-                FillBarColorChange.UpdateVisual(m_Jetpack.CurrentFillRatio);
-            }
-        }
-    }
-}
+//             if (m_Jetpack.IsJetpackUnlocked)
+//             {
+//                 JetpackFillImage.fillAmount = m_Jetpack.CurrentFillRatio;
+//                 FillBarColorChange.UpdateVisual(m_Jetpack.CurrentFillRatio);
+//             }
+//         }
+//     }
+// }
